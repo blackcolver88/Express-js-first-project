5054d3e82e67da97f753ca59399ede33f8723073
Moemen <117341508+blackcolver88@users.noreply.github.com>
2024-03-01 12:58:16 +0100

    Initial setup

---
+++ server.js
@@ -0,0 +1,74 @@
+const express= require('express');
+const User=require('./models/user')
+require('./config/connect')
+const app=express();
+app.use(express.json());
+app.listen(3000,()=>{
+    console.log("hello");
+});
+app.post('/add',(req ,res)=>{
+    data =req.body;
+    usr= new User(data);
+    usr.save()
+    .then((savedUser)=>{res.send(savedUser)})
+    .catch((err)=>{res.send(err)})
+
+});
+app.post('/create', async (req ,res)=>{
+    try
+    {
+        data =req.body;
+        usr= new User(data);
+        savedUser = await usr.save();
+        res.send(savedUser);
+    } catch (error) {res.send(error)}
+    });
+app.get('/get_all',(req,res)=>{
+    User.find()
+        .then((users)=>{res.send(users);})
+        .catch((err)=>{res.send(err);})
+});
+app.get('/all',async (req,res)=>{
+    try{
+        users = await User.find();
+        res.send(users);
+
+    }catch (error) {res.send(error);}
+});
+app.get('/getbyid/:id', (req,res)=>{
+    id=req.params.id;
+    User.findOne({_id:id})
+    .then((user)=>{res.send(user)})
+    .catch((err)=>{res.send(err)})
+});
+app.get('/getByid/:id', async (req,res)=>{
+    try{
+        id=req.params.id;
+        us = await User.findOne({_id:id});
+        res.send(us);
+
+    }catch (error) {res.send(error); }
+});
+app.delete('/delete/:id',(req,res)=>{
+    id =req.params.id;
+    User.findByIdAndDelete({_id:id})
+    .then((deletedUser)=>{res.send(deletedUser)}
+    ).catch((err)=>{res.send(err)})
+});
+app.delete('/deleter/:id', async (req,res)=>{
+    try{
+    id =req.params.id;
+    del = await User.findByIdAndDelete({_id:id});
+    res.send(del);
+    }catch(error){res.send(error);}
+});
+app.put('/update/:id',(req,res)=>{
+    id=req.params.id;
+    newData = req.body;
+    User.findByIdAndUpdate({_id:id}, newData)
+    .then((updated)=>{res.send(updated)})
+    .catch((err)=>{res.send(err)})
+});
+
+
+
